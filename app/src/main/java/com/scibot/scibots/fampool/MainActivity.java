f2e1235f5a25e4f21dcc9c6ba0beb7d7b545daaa
aniket sharma <aniket965.as@gmail.com>
2017-04-18 21:11:21 +0530

    added contacts liste

--- app/src/main/java/com/scibot/scibots/fampool/MainActivity.java
+++ app/src/main/java/com/scibot/scibots/fampool/MainActivity.java
@@ -1,7 +1,16 @@
 package com.scibot.scibots.fampool;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Build;
+
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> email =  new ArrayList<>();
+
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                Cursor cur1 = cr.query(
+                        ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                        new String[]{id}, null);
+
+
+                while (cur1.moveToNext()) {
+
+
+                    String name=cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    Log.e("Name :", name);
+                    String emails = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                    Log.e("Email", emails);
+                    if(emails != null){
+                        names.add(name);
+                        email.add(emails);
+
+                    }
+                }
+                cur1.close();
+            }
+        }
+
+
+
+
     }
+
+
 }
